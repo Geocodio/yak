f367548e0eb034b86a87135b75d0705d49b533d7
Mathias Hansen <me@codemonkey.io>
2026-04-23 12:51:20 +0200

    feat(deployments): register hibernate + sweep schedules

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Channels\GitHub\PollPullRequestReactionsJob;
+use App\Jobs\Deployments\HibernateIdleDeploymentsJob;
+use App\Jobs\Deployments\SweepExpiredDeploymentsJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -22,3 +24,5 @@ Schedule::command('yak:healthcheck')->everyFifteenMinutes();
 Schedule::command('yak:scan-ci')->everyTwoHours();
 Schedule::command('yak:poll-drone-ci')->everyMinute()->withoutOverlapping();
 Schedule::job(PollPullRequestReactionsJob::class)->hourly()->name('poll-pr-reactions');
+Schedule::job(HibernateIdleDeploymentsJob::class)->everyMinute()->name('deployments:hibernate-idle')->withoutOverlapping();
+Schedule::job(SweepExpiredDeploymentsJob::class)->hourly()->name('deployments:sweep-expired')->withoutOverlapping();
