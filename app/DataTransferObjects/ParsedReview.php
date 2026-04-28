6c3bf12d71b3244967566b5137d933b477bbaab5
Mathias Hansen <me@codemonkey.io>
2026-04-28 11:39:08 +0200

    feat(pr-review): add priorFindings to ParsedReview

--- app/DataTransferObjects/ParsedReview.php
+++ app/DataTransferObjects/ParsedReview.php
@@ -6,11 +6,13 @@ final readonly class ParsedReview
 {
     /**
      * @param  array<int, ReviewFinding>  $findings
+     * @param  array<int, ParsedPriorFinding>  $priorFindings
      */
     public function __construct(
         public string $summary,
         public string $verdict,
         public string $verdictDetail,
         public array $findings,
+        public array $priorFindings = [],
     ) {}
 }
